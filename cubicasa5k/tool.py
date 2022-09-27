afcae3e4dd0c302a534092aeed93cda9ae0e86c4
agaitanis <alexandros.gaitanis@gmail.com>
2022-09-27 18:15:59 +0300

    Open a file manager for selecting images

--- cubicasa5k/tool.py
+++ cubicasa5k/tool.py
@@ -1,27 +1,41 @@
 import sys
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
 
 
 class MainWin(QMainWindow):
     def __init__(self):
         super().__init__()
+        self._file_dialog = QFileDialog(self)
+        self._create_win()
 
-        self.setWindowTitle("Floor Plan Recognition for Evacuation")
+
+    def _create_win(self):
+        self.setWindowTitle("Floor Plan Recognition")
         self.setMinimumSize(QSize(800, 600))
 
         open_action = QAction("Open", self)
         open_action.setShortcut("Ctrl+O")
         open_action.setStatusTip("Open image")
-        open_action.triggered.connect(self.open_image)
+        open_action.triggered.connect(self._open_image)
 
         menu_bar = self.menuBar()
         file_menu = menu_bar.addMenu("&File")
         file_menu.addAction(open_action)
+    
+
+    def _draw_image(self, fname):
+        pass # FIXME
 
 
-    def open_image(self):
-        print("open_image called")
+    def _open_image(self):
+        self._file_dialog = QFileDialog(self)
+        self._file_dialog.setWindowTitle("Open Image")
+        self._file_dialog.setNameFilter("Images (*.png)")
+
+        if self._file_dialog.exec() == QFileDialog.Accepted:
+            fname = self._file_dialog.selectedFiles()[0]
+            self._draw_image(fname)
 
 
 def main():
